c7f233b2a661e8c6991a123b857ed681a1051469
tomyItSky <tomy.agung@skyparking.co.id>
2024-07-04 16:17:48 +0700

    .

--- src/pages/Topup.jsx
+++ src/pages/Topup.jsx
@@ -141,62 +141,64 @@ export default function Topup() {
           initial={{ y: "100%" }}
           animate={{ y: 0 }}
           transition={{ duration: 0.5 }}
-          className="fixed bottom-0 left-0 w-full bg-white p-5 shadow-2xl rounded-t-3xl border border-slate-400 z-20"
+          className="container fixed inset-0 bg-black bg-opacity-50 z-20 flex items-end justify-center"
         >
-          <h2 className="text-base text-slate-400 font-medium mb-1 mt-20">
-            Nominal Topup
-          </h2>
-          <h1 className="text-4xl font-medium">
-            <span className="font-semibold">IDR</span>{" "}
-            {amount.toLocaleString("id-ID")}
-          </h1>
-
-          <div className="flex justify-between items-center mt-5 border-b border-gray-300 pb-2 pt-3">
-            <div className="text-base text-gray-400">Metode pembayaran</div>
-            <p className="font-semibold">{selectedMethod}</p>
-          </div>
-
-          <div className="flex justify-between items-center border-b border-gray-300 pb-2 pt-3">
-            <div className="text-base text-gray-400">Biaya admin</div>
-            <p className="font-semibold">
+          <div className="bg-white w-full max-h-[90vh] overflow-y-auto p-5 shadow-2xl rounded-t-3xl border border-slate-400">
+            <h2 className="text-base text-slate-400 font-medium mb-1 mt-20">
+              Nominal Topup
+            </h2>
+            <h1 className="text-4xl font-medium">
               <span className="font-semibold">IDR</span>{" "}
-              {parseInt("5000").toLocaleString("id-ID")}
-            </p>
-          </div>
+              {amount.toLocaleString("id-ID")}
+            </h1>
 
-          <div className="flex justify-between items-center border-b border-gray-300 pb-2 pt-3">
-            <div className="text-base text-gray-400">Total points</div>
-            <p className="font-semibold">{amount / 1000} Points</p>
-          </div>
+            <div className="flex justify-between items-center mt-5 border-b border-gray-300 pb-2 pt-3">
+              <div className="text-base text-gray-400">Metode pembayaran</div>
+              <p className="font-semibold">{selectedMethod}</p>
+            </div>
 
-          <div className="flex justify-between items-center border-b border-gray-300 pb-2 pt-3">
-            <div className="text-base text-gray-400">Tanggal</div>
-            <p className="font-semibold">
-              {format(new Date(), "dd MMM yy HH:mm:ss")}
-            </p>
-          </div>
+            <div className="flex justify-between items-center border-b border-gray-300 pb-2 pt-3">
+              <div className="text-base text-gray-400">Biaya admin</div>
+              <p className="font-semibold">
+                <span className="font-semibold">IDR</span>{" "}
+                {parseInt("5000").toLocaleString("id-ID")}
+              </p>
+            </div>
 
-          <div className="flex flex-col justify-center items-center space-y-1">
-            <button
-              className="bg-blue-500 text-white py-2 px-4 rounded-lg mt-5 w-full"
-              onClick={verifikasi}
-            >
-              Lanjutkan
-            </button>
-            <button
-              className="bg-red-500 text-white py-2 px-4 rounded-lg mt-5 w-full"
-              onClick={closeModal}
-            >
-              Batal
-            </button>
-          </div>
-          <div className="absolute -top-16 left-1/2 transform -translate-x-1/2">
-            <div className="relative w-36 h-36 bg-blue-600 opacity-40 rounded-full"></div>
-            <div className="absolute inset-0 w-24 h-24 bg-blue-600 opacity-100 rounded-full m-auto">
-              <TbExclamationMark
-                size={50}
-                className="m-auto mt-5 text-amber-500"
-              />
+            <div className="flex justify-between items-center border-b border-gray-300 pb-2 pt-3">
+              <div className="text-base text-gray-400">Total points</div>
+              <p className="font-semibold">{amount / 1000} Points</p>
+            </div>
+
+            <div className="flex justify-between items-center border-b border-gray-300 pb-2 pt-3">
+              <div className="text-base text-gray-400">Tanggal</div>
+              <p className="font-semibold">
+                {format(new Date(), "dd MMM yy HH:mm:ss")}
+              </p>
+            </div>
+
+            <div className="flex flex-col justify-center items-center space-y-1">
+              <button
+                className="bg-blue-500 text-white py-2 px-4 rounded-lg mt-5 w-full"
+                onClick={verifikasi}
+              >
+                Lanjutkan
+              </button>
+              <button
+                className="bg-red-500 text-white py-2 px-4 rounded-lg mt-5 w-full"
+                onClick={closeModal}
+              >
+                Batal
+              </button>
+            </div>
+            <div className="absolute -top-16 left-1/2 transform -translate-x-1/2">
+              <div className="relative w-36 h-36 bg-blue-600 opacity-40 rounded-full"></div>
+              <div className="absolute inset-0 w-24 h-24 bg-blue-600 opacity-100 rounded-full m-auto">
+                <TbExclamationMark
+                  size={50}
+                  className="m-auto mt-5 text-amber-500"
+                />
+              </div>
             </div>
           </div>
         </motion.div>
