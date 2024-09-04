de3ad0ca5b517a1e545dbe79838e33d24854844b
tomyItSky <tomy.agung@skyparking.co.id>
2024-09-04 08:06:42 +0700

    .

--- src/pages/Mobile/components/HistoryPayment.jsx
+++ src/pages/Mobile/components/HistoryPayment.jsx
@@ -13,14 +13,64 @@ export default function HistoryPayment({ listRiwayat }) {
       currency: "IDR",
     });
   };
+  console.log(listRiwayat);
   return (
     <>
       {listRiwayat.map((items, index) => (
         <div
           key={index}
-          className="flex flex-row justify-between items-center bg-white shadow-md w-full py-2 rounded-lg px-2"
+          className="bg-white shadow-md w-full rounded-lg border border-slate-300 text-xs relative h-60"
         >
-          <div className="flex flex-row justify-center items-center space-x-3 py-2">
+          <div className="absolute bg-white rounded-full h-6 w-6 -left-4 top-7 border-r border-slate-300"></div>
+          <div className="absolute bg-white rounded-full h-6 w-6 -right-4 top-7 border-l border-slate-300"></div>
+
+          <div className="flex justify-between items-center w-full mb-3 border-b border-dashed py-3 px-3">
+            <h1>{items.Activity.toUpperCase()}</h1>
+            <h3>{format(new Date(items.CreatedAt), "dd MMM yy")}</h3>
+          </div>
+
+          <div className="flex justify-between items-center w-full mb-3 border-b py-3 px-3">
+            <div className="flex flex-col justify-start items-start">
+              <label htmlFor="plat_nomor" className="text-xs text-slate-400">
+                Plat Nomor
+              </label>
+              <h1 className="text-sm font-semibold">tambahkan plat</h1>
+            </div>
+            <div className="flex flex-col justify-start items-start w-40">
+              <label htmlFor="plat_nomor" className="text-xs text-slate-400">
+                {items.Activity === "topup" ? "Price" : "Lokasi"}
+              </label>
+              <h1 className="text-xs font-semibold text-start">
+                {items.Activity === "topup"
+                  ? formatCurrency(items.Price)
+                  : items.LocationName}
+              </h1>
+            </div>
+          </div>
+
+          <div className="flex justify-between items-center w-full mb-3 px-3 ">
+            <p
+              className={`text-xs font-semibold border px-3 py-2 rounded-xl ${
+                items.Status === "Success"
+                  ? "text-green-600 bg-green-100"
+                  : items.Status === "Pending"
+                  ? "text-yellow-600 bg-yellow-100"
+                  : items.Status === "Cancel"
+                  ? "text-red-600 bg-red-100"
+                  : "-"
+              }`}
+            >
+              {items.Status === "Success"
+                ? items.Status.toUpperCase()
+                : items.Status === "Pending"
+                ? items.Status.toUpperCase()
+                : items.Status === "Cancel"
+                ? items.Status.toUpperCase()
+                : "-"}
+            </p>
+          </div>
+
+          {/* <div className="flex flex-row justify-center items-center space-x-3 py-2">
             {items.Activity === "topup" ? (
               <MdOutlineAccountBalanceWallet
                 size={30}
@@ -75,7 +125,7 @@ export default function HistoryPayment({ listRiwayat }) {
             <h1 className="text-xs font-medium">
               {format(new Date(items.CreatedAt), "HH:mm:ss")}
             </h1>
-          </div>
+          </div> */}
         </div>
       ))}
     </>
