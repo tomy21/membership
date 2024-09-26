00de8128d9fe572ffb5f0fc1ca31937d051a910d
tomyItSky <tomy.agung@skyparking.co.id>
2024-09-26 15:01:00 +0700

    .

--- src/pages/Mobile/pages/Membership.jsx
+++ src/pages/Mobile/pages/Membership.jsx
@@ -74,6 +74,11 @@ export default function Membership() {
           const response = await productBundleAll.getByIdProduct(
             selectedLocation.IdProduct
           );
+          const responseQuota = await productBundleAll.getProductQuote(
+            selectedLocation.IdProduct
+          );
+          setPeriodId(responseQuota.data[0].Id ?? responseQuota.data.Id);
+          console.log(responseQuota.data[0].Id ?? responseQuota.data.Id);
           setDataLocation(response.data);
         } catch (error) {
           console.error("Failed to fetch product data:", error);
@@ -117,16 +122,9 @@ export default function Membership() {
             selectedVehicleType.Code
           );
 
-          const responseQuota = await productBundleAll.getProductQuote(
-            selectedVehicleType.Code
-          );
-
-          console.log(responseBundle.data.Price);
-
+          setTariff(responseBundle.data.Price);
           setProductBundle(responseBundle?.data);
           setProductId(responseBundle.data.MemberProductId);
-          setPeriodId(responseQuota.data[0].Id ?? responseQuota.data.Id);
-          setTariff(responseBundle.data.Price);
           setStartDate(responseBundle.data?.StartDate);
           setEndDate(responseBundle.data?.EndDate);
         } catch (error) {
@@ -312,7 +310,9 @@ export default function Membership() {
                 placeholder="0.00"
                 className="block w-full rounded-md border-0 py-3 pl-16 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 value={
-                  tariff ? `${parseInt(tariff).toLocaleString("id-ID")}` : "-"
+                  tariff !== 0
+                    ? `${parseInt(tariff).toLocaleString("id-ID")}`
+                    : "Price not available"
                 }
                 disabled
               />
