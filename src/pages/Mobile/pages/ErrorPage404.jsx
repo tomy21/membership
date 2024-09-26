2964130fedc14a2a1cf9576b8e660f3b45e79e68
tomyItSky <tomy.agung@skyparking.co.id>
2024-09-26 08:48:57 +0700

    .

--- src/pages/Mobile/pages/ErrorPage404.jsx
+++ src/pages/Mobile/pages/ErrorPage404.jsx
@@ -6,7 +6,7 @@ export default function ErrorPage404() {
       <div className="flex flex-col justify-center items-center h-screen gap-y-5">
         <img src={"/assets/404-error.png"} className="w-40" alt="" />
         <h1 className="text-2xl">
-          Maaf halaman ini belum dapat di akses melalui dekstop
+          Maaf halaman ini belum dapat di akses melalui desktop
         </h1>
       </div>
     </>
